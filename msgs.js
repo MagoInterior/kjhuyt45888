4f4d51d1d4993724ae95c0f5496d889ec09fac66
Botssh <audienciacerta45@gmail.com>
2022-09-25 06:07:36 -0300

    first commit

---
+++ msgs.js
@@ -0,0 +1,84 @@
+module.exports = configMsg = async (conn, body, reply, from) => {
+
+switch(body.toLowerCase()){
+case "menu":
+reply('🤖 - Bem-vindo(a) ao atendimento automático, me chamo *R2D2* sou assistente virtual do *TIAGO* \n\n🤖 -  Envie o número da opção desejada.\n🤖 -  0 •FORMAS DE PAGAMENTO. \n🤖 -  1 •VALORES DO LOGIN PREMIUM\n🤖 -  2 •COMPRAR LOGIN OU RENOVAR\n🤖 -  3 •GERAR UM TESTE PREMIUM\n🤖 -  4 •BAIXAR O APP\n🤖 -  5 •VALORES PARA REVENDA\n🤖 -  6 •RENOVAR REVENDA\n🤖 -  100 •TERMOS E CONDIÇÕES')
+break
+case "0":reply("💎✨ ғᴏʀᴍᴀs ᴅᴇ ᴘᴀɢᴀᴍᴇɴᴛᴏ:\n\nPix  💠✨\n\nPic Pay 💠✨\n\nMercado Pago💠✨\n\nMENU Voltar") 
+break 
+case "1":
+reply('🗣️ _A Speed Net chegou para inovar!! Internet ILIMITADA disponibilizada pelo nosso *aplicativo*, onde liberamos um *usuário e senha* para fazer a conexão._ \n\n📲✨ _Disponível para *ANDROID*. É necessário que possua chip da *TIM, OI ou VIVO*._ \n\n💱 _Os valores são decorrentes a quantidades de acessos que você precisar e a renovação é Mensal._\n\nTESTE GRÁTIS DE 1H \n\nTABELA DE PREÇO 👇 \n\n*R$20,00💵 - 1 acesso📶* \n\n*R$27,00💵 - 2 acesso📶* \n\n*R$35,00💵 - 3 acesso📶* \n\n☆   ☆   ☆   ☆   ☆   ☆   ☆ \nVÁLIDOS POR 30 DIAS \n\n✅Download ilimitado⬇️\n✅Internet ilimitada 🔄\n❌Torrente ⛓\n\nVENHA SER MAIS UM CLIENTE NOSSO🥳 \n\n3️⃣PEDIR TESTE\n0️⃣FORMAS DE PAGAMENTO \n*Menu* VOLTAR AO MENU')
+break 
+case "2":
+reply("Pix: tiagolaurentino358@gmail.com \n(Chave E-mail) ✉️ \n\nNome :Tiago L da rocha \nBanco: C6 Bank ⚫\n\nNÃO ESQUEÇA O COMPROVANTE 📃\n\nCaso seja o seu primeiro Login e quiser receber automaticamente so entra no bot do telegram e pegar seu login por la.\nLink do bot http://t.me/storesshvpn_bot") 
+break 
+case "3":
+reply("Para gerar seu teste entre no telegram. \nDepois só clicar e gerar Teste\nCaso queira comprar um Login la vai te também, Lembrando que o suporte vai ser por aqui pelo whatsapp\n\nLink do bot aqui👇🏼👇🏼\nhttp://t.me/storesshvpn_bot") 
+break 
+case "4":
+reply("Por favor instale esse App no seu celular 📱👇👇👇\nAPLICATIVO 1- SPEED NET 5G PRIMEIRO BAIXE O APP PELO GOOGLE DRIVE \n\nhttps://drive.google.com/file/d/1Qcm_JhYsT-qSYyB5lYxhJIgiF1X9bNt8/view?usp=drivesdk \n\nENTRA NELE COM WIFI , ELE VAI PEDIR PRA ATUALIZAR \n\nAPLICATIVO 2- SPEED TUNNEL PRIMEIRO BAIXE O APP PELO GOOGLE DRIVE \n\nhttps://drive.google.com/file/d/1S08SIzYK7qMJfa2SPBfm8yUsZSHDc--e/view?usp=drivesdk \n\nENTRA NELE COM WIFI , ELE VAI PEDIR PRA ATUALIZAR") 
+break 
+
+case "5":
+reply("QUER SER UM REVENDEDOR? \n\n🧩BASIC PLANS : \n≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈\n✓ = 10 𝚕𝚘𝚐𝚒𝚗𝚜 = 30,00𝚁$\n✓ = 20 𝚕𝚘𝚐𝚒𝚗𝚜 = 50,00𝚁$ \n✓ = 30 𝚕𝚘𝚐𝚒𝚗𝚜 = 70,00𝚁$ \n✓ = 40 𝚕𝚘𝚐𝚒𝚗𝚜 = 90,00𝚁$\n✓ = 80 𝚕𝚘𝚐𝚒𝚗𝚜 = 170,00𝚁$\n≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈≈ \n\n👤Contact Telegram: https://t.me/internetilimitada4587 \n⚜️Referência: https://t.me/speednetvip9 \n\n*Menu* Voltar ao menu principal \nPara Formas de pagamento") 
+break 
+
+case "6":
+reply("🤖Digite *2* Para receber a chave PIX.\nNÃO ESQUEÇA O COMPROVANTE 📃\n\nOps: Voce só pode renovar seu painel quando tiver faltando 2 dias ou menos.\nCaso queira adicionar mais Logins no painel, cada login adicionado é 2 Real.")
+break 
+case "100":
+reply(" *Termos e Condições para Usuário de VPN* \n\n- Você usa este tipo de app como alternativa para ter acesso a conteudos bloqueados na sua rede, o serviço de internet funciona bem, porém é sujeito a vários problemas, como lentidão, quedas e etc, por ser um meio barato de conexão, esteja ciente disso ao usar! \n- Não somos responsáveis pela rede na qual você acessa nosso serviço, qualquer problema no chip/rede/sinal/wi-fi não é de nossa responsabilidade. \n- Você é responsável por manter sua rede no qual faz a conexão ativa. \n- Você é responsável por manter seu app/arquivo atualizados para sempre ter melhor funcionamento.\n- *Reembolso* Só se a Internet Não funcionar em até 7 dias, Passando disso voce não tem acesso o reembolso.")
+break
+
+
+case "comandoadm":
+reply(`
+╭───────────────
+╎
+┝  ⎙ Menu De Administradores
+╎
+╰──────────
+╎
+╎⩺ /Kick [@] (pra-remover) 
+╎⩺ /Ban (responder-mensagem)
+╎⩺ /Promover [@] (Ser-ADM)
+╎⩺ /Rebaixar [@] (rebaixar-adm)
+╎⩺ /Totag (menciona-algo)
+╎⩺ /Grupo f/a
+╎⩺ /Status
+╎⩺ /Limpar (texto-invisível-gp)
+╎⩺ /Atividades (DO-GRUPO)
+╎⩺ /Linkgp
+╎⩺ /Grupoinfo
+╎⩺ /Hidetag (txt) (marcação)
+╎⩺ /Marcar (marca tds do gp)
+╎⩺ /Marcar2 (Marca-tds-Wa.me/)
+╎⩺ /Anagrama 1 / 0
+╎⩺ /Autofigu 1 / 0
+╎⩺ /Antidocumento 1 / 0  
+╎⩺ /Antipalavra 1 / 0
+╎⩺ /Antiloc 1 / 0  
+╎⩺ /Anticontato 1 / 0  
+╎⩺ /Antilink 1 / 0
+╎⩺ /Antilinkhard 1 / 0
+╎⩺ /AntiCatalogo 1 / 0
+╎⩺ /Antifake 1 / 0
+╎⩺ /Bemvindo 1 / 0
+╎⩺ /Antiimg 1 / 0
+╎⩺ /Antiaudio 1 / 0
+╎⩺ /Antivideo 1 / 0
+╎⩺ /Leveling 1 / 0  
+╎⩺ /Simih 1 / 0
+╎⩺ /Simih2 1 / 0
+╎⩺ /Fotogp (Marca)
+╎⩺ /Descgp (TXT)
+╎⩺ /Nomegp (Nome)
+╎⩺ /Criartabela (ESCREVA-ALGO)
+╎⩺ /Tabelagp
+╎
+╰─────────────╯
+`)
+break
+}
+
+}
